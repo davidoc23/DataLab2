1331065430a225c752cb8687fbe6541e4f1126fc
davidoc23 <g00400530@atu.ie>
2023-10-09 18:52:35 +0100

    lab 2 complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+// Importing the necessary CSS and components
 import './App.css';
-import Header from './Components/Header'
+import Header from './Components/Header';
 import Content from './Components/content';
 import Footer from './Components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,29 +9,34 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 
-
+// Define the main App component
 function App() {
   return (
+    // Wrapping the entire app with BrowserRouter for client-side routing
     <BrowserRouter>
       <div className="App">
-          <Navbar bg="primary" data-bs-theme="dark">
-            <Container>
-              <Navbar.Brand href="/">Navbar</Navbar.Brand>
-              <Nav className="me-auto">
+        {/* Navbar component with links */}
+        <Navbar bg="primary" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/read">Read</Nav.Link>
               <Nav.Link href="/create">Create</Nav.Link>
-              </Nav>
-            </Container>
-          </Navbar>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        {/* Defining routes for rendering different components */}
         <Routes>
-          <Route path='/' element={<Content />} exact />
-          <Route exact path='/create' element={<Header />} />
-          <Route path='/read' element={<Footer />} />
+          <Route path='/' element={<Content />} exact />  {/* Home page */}
+          <Route exact path='/create' element={<Header />} />  {/* Create page */}
+          <Route path='/read' element={<Footer />} />  {/* Read page */}
         </Routes>
       </div>
     </BrowserRouter>
   );
 }
 
-  export default App;
+// Export the App component as the default export
+export default App;
